e82ca6f2eee1e2f577edaa7c120b999b61fb5df8
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-05 09:17:58 -0600

    Add listener to button to fire event when button is pressed.

--- labAndroidTutorial/app/src/main/java/com/danthesalmon/labandroidtutorial/MainActivity.java
+++ labAndroidTutorial/app/src/main/java/com/danthesalmon/labandroidtutorial/MainActivity.java
@@ -3,12 +3,12 @@ package com.danthesalmon.labandroidtutorial;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     TextView mainTextView;
     Button mainButton;
@@ -21,6 +21,10 @@ public class MainActivity extends ActionBarActivity {
         // Access the TextView defined in activity_main.xml
         mainTextView = (TextView) findViewById(R.id.main_textview);
         mainTextView.setText("Set in the code!");
+
+        //Access the Button defined in activity_main.xml and list for presses
+        mainButton = (Button) findViewById(R.id.main_button);
+        mainButton.setOnClickListener(this);
     }
 
 
@@ -29,5 +33,10 @@ public class MainActivity extends ActionBarActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    public void onClick(View v) {
+        mainTextView.setText("Button presssed!");
     }
 }

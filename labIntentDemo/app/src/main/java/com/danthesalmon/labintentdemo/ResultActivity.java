daa5588f5096ffac5b37e6944d32ff3e81ad34b2
Dan Salmon <dan.sa7mon@gmail.com>
2015-02-24 10:27:03 -0600

    Add new class

---
+++ labIntentDemo/app/src/main/java/com/danthesalmon/labintentdemo/ResultActivity.java
@@ -0,0 +1,16 @@
+package com.danthesalmon.labintentdemo;
+
+/**
+ * Created by Dan on 2/24/2015.
+ */
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ResultActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_result);
+    }
+}
